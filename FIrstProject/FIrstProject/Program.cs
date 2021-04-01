17c0852eff01d562f57f4c702b18fea6ca24528a
Oleg Shornikov <soleg1250@gmail.com>
2021-04-01 21:16:33 +0300

    Use proper grammar.

--- FIrstProject/FIrstProject/Program.cs
+++ FIrstProject/FIrstProject/Program.cs
@@ -9,7 +9,7 @@ namespace FIrstProject
             string name;
             Console.WriteLine("Enter your name: ");
             name = Console.ReadLine();
-            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Hello, {0}", name);
         }
     }
 }
